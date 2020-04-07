80402366edb03fe47020a2191532162c835d2cc9
AdrianAlbrecht <155376@student.uwm.edu.pl>
2020-04-07 20:51:42 +0200

    Lista zadan nr 3 POPRAWKI

--- cw3_zad6.py
+++ cw3_zad6.py
@@ -1,4 +1,5 @@
+from math import sqrt
 def promien_okregu(x,y,a=0,b=0):
-    return(x-a)**2+(y-b)**2
-print(promien_okregu(2,2))
+    return sqrt((x-a)**2+(y-b)**2)
+print(promien_okregu(7,4))
 print(promien_okregu(2,2,1,1))
