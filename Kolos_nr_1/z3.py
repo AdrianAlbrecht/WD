0d034de81e3df9c60b29744cc1c8ff76aff6703f
AdrianAlbrecht <155376@student.uwm.edu.pl>
2020-05-14 14:37:16 +0200

    K1

--- Kolos_nr_1/z3.py
+++ Kolos_nr_1/z3.py
@@ -1,5 +1,6 @@
 import random as rd
 
+#ściezka jakiej musiałem użyć do pliku, aby mi zadziałało: './Kolos_nr_1/z3.txt'
 plik=open("z3.txt","w")
 for i in range(20):
     li=rd.randint(1,100000)
--- Kolos_nr_1/z8.py
+++ Kolos_nr_1/z8.py
@@ -1,11 +1,15 @@
-plik=open('z8.txt','r')
-imiona={}
-for line in plik:
-    li=line.split("\n")
-    if li[0] in imiona.keys():
-        imiona[str(li[0])]+=1
-    else:
-        imiona[str(li[0])]=1
-for key,value in imiona.items():
-    print("{}: {}".format(key,imiona[key]))
-plik.close()
+#ściezka jakiej musiałem użyć do pliku, aby mi zadziałało: './Kolos_nr_1/z8.txt'
+def z8():
+    plik=open('z8.txt','r')
+    imiona={}
+    for line in plik:
+        li=line.split("\n")
+        if li[0] in imiona.keys():
+            imiona[str(li[0])]+=1
+        else:
+            imiona[str(li[0])]=1
+    for key,value in imiona.items():
+        print("{}: {}".format(key,value))
+    plik.close()
+
+z8()
